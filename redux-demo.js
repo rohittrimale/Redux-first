ae954510e8e80908f117efdd4f8e5f48bc87402c
rohittrimale <rohittrimalle@gmail.com>
2024-06-09 15:37:33 +0530

    Redux- basic Code

---
+++ redux-demo.js
@@ -0,0 +1,28 @@
+const redux = require("redux");
+
+const couterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = redux.createStore(couterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "decrement" });
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
